5e73944588c60001eb40013f4e895e74c61bc7aa
Cloudlightcorp <242549818+Cloudlightcorp@users.noreply.github.com>
2025-11-30 13:55:50 +0530

    Modify macOS EC2 host and instance settings
    Updated EC2 host and instance configurations for macOS.

--- main.tf
+++ main.tf
@@ -1,41 +1,32 @@
-terraform {
-  backend "s3" {
-    bucket = "automated-lab-ec2-instances"
-    key    = "AutomatedLab-ec2-state-mac/terraform.tfstate"
-    region = "us-west-2"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-west-2"
 }
 
-############################################
-# MAC DEDICATED HOST (MANDATORY)
-############################################
-resource "aws_ec2_host" "mac_host" {
-  instance_type     = "mac2-m1.metal"
-  availability_zone = "us-west-2a"
-  auto_placement    = "on"
+terraform {
+  backend "s3" {
+    bucket = "automated-lab-ec2-instances"
+    key    = "AutomatedLab-ec2-state-Mac/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
-############################################
-# MAC EC2 INSTANCE
-############################################
-resource "aws_instance" "mac_ec2" {
-  ami                    = "ami-0f8c45225ea6a7598"
-  instance_type          = "mac2-m1.metal"
-  host_id                = aws_ec2_host.mac_host.id
-  key_name               = "Lab_env_Mac"
+# Dedicated Mac Host (required)
+resource "aws_ec2_host" "mac_host" {
+  availability_zone = "us-west-2a"
+  instance_type     = "mac1.metal"
+  quantity          = 1
+}
 
-  user_data = file("${path.module}/scripts/mac_install_apps.sh")
+# macOS EC2 instance
+resource "aws_instance" "mac_instance" {
+  ami                         = "ami-05c1e7b8b59cfe34f" # mac1 macOS Intel AMI
+  instance_type               = "mac1.metal"
+  availability_zone           = "us-west-2a"
+  host_id                     = aws_ec2_host.mac_host.id
+  key_name                    = "Lab_env_Mac"
+
+  # macOS script
+  user_data = file("${path.module}/scripts/install_macos_tools.sh")
 
   tags = {
     Name      = "lab-env-ec2-macos"
