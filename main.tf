6f5f9dbe13364dd637e55815742fa0d92c192df8
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-12-01 19:25:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "mac_instance" {
   ami           = "ami-0c4d1b01002bc5424" # mac2 M1 AMI for us-east-1
   instance_type = "mac2.metal"
-  key_name      = "Lab_env_Mac"
+  key_name      = "Lab_env_Mac(test)"
 
   user_data = file("${path.module}/scripts/install_macos_tools.sh")
 
