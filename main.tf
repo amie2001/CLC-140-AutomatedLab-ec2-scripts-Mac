80c9b8225d6381ddbb80d561c6ed6379061029e0
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-12-01 18:54:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-southeast-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "automated-lab-ec2-instances"
-    key    = "AutomatedLab-ec2-state-Mac/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
-
-# Dedicated Mac Host (required)
-resource "aws_ec2_host" "mac_host" {
-  availability_zone = "us-west-2a"
-  instance_type     = "mac1.metal"
-}
-
-# macOS EC2 instance
 resource "aws_instance" "mac_instance" {
-  ami                         = "ami-05c1e7b8b59cfe34f" # mac1 macOS Intel AMI
-  instance_type               = "mac1.metal"
-  availability_zone           = "us-west-2a"
-  host_id                     = aws_ec2_host.mac_host.id
-  key_name                    = "Lab_env_Mac"
+  ami           = "ami-0f8b0d25840c5e691" # mac2 M1 AMI Singapore
+  instance_type = "mac2-m1.metal"
+  key_name      = "Lab_env_Mac"
 
-  # macOS script
   user_data = file("${path.module}/scripts/install_macos_tools.sh")
 
   tags = {
