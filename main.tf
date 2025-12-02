5071722224969dbda20dd1102f42da8d3e04cbc4
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-12-02 09:02:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,28 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "mac_instance" {
-  ami           = "ami-0c4d1b01002bc5424" # mac2 M1 AMI for us-east-1
-  instance_type = "mac2.metal"
-  tenancy           = "host"                        # 🔥 MANDATORY
-  host_id           = aws_ec2_host.mac_host.id
-  key_name      = "Lab_env_Mac(test)"
+#####################################################
+# Dedicated Host for macOS (Mandatory for mac EC2)
+#####################################################
+resource "aws_ec2_host" "mac_host" {
+  availability_zone = "ap-southeast-1a"
+  instance_type     = "mac2-m1.metal"   # REQUIRED for mac2 AMI
+}
 
+#####################################################
+# macOS EC2 instance on that Dedicated Host
+#####################################################
+resource "aws_instance" "mac_instance" {
+  ami               = "ami-0c4d1b01002bc5424"
+  instance_type     = "mac2-m1.metal"
+  availability_zone = "ap-southeast-1a"
+
+  tenancy           = "host"                      # MUST
+  host_id           = aws_ec2_host.mac_host.id    # MUST
+
+  key_name          = "Lab_env_Mac(test)"
+
+  # macOS installation script
   user_data = file("${path.module}/scripts/install_macos_tools.sh")
 
   tags = {
