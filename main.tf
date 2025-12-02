1ec14c51fb82ff691677235709529177cfd1eb0e
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-12-02 08:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_instance" "mac_instance" {
   ami           = "ami-0c4d1b01002bc5424" # mac2 M1 AMI for us-east-1
   instance_type = "mac2.metal"
+  tenancy           = "host"                        # 🔥 MANDATORY
+  host_id           = aws_ec2_host.mac_host.id
   key_name      = "Lab_env_Mac(test)"
 
   user_data = file("${path.module}/scripts/install_macos_tools.sh")
